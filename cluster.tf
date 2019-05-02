bef12c4322e6ecda76d552705117d02927e357af
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 13:30:37 -0500

    actual modeules

--- cluster.tf
+++ cluster.tf
@@ -1,17 +1,16 @@
 module "primary-cluster" {
-  name               = "my-cluster"
-  source             = "russmedia/kubernetes-cluster/google"
-  version            = "2.0.0"
-  region             = "us-east1"
-  zones              = "us-east4-b"
-  project            = "linux-classs"
-  environment        = "Dev"
-  min_master_version = "1.11.8"
+  name                                 = "my-cluster"
+  source                               = "russmedia/kubernetes-cluster/google"
+  version                              = "2.0.0"
+  region                               = "us-east1"
+  zones                                = "us-east4-b"
+  project                              = "linux-classs"
+  environment                          = "Dev"
+  min_master_version                   = "1.11.8"
   network                              = "default"
   nodes_subnet_ip_cidr_range           = "10.101.0.0/24"
   nodes_subnet_container_ip_cidr_range = "172.21.0.0/16"
   nodes_subnet_service_ip_cidr_range   = "10.201.0.0/16"
-}
 
   #node_pools             = ""
 
