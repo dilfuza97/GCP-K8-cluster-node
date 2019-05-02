56a2f0800629a0f281eac971e37ea57c4726ec47
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-02 13:18:38 -0500

    actual modeules

--- cluster.tf
+++ cluster.tf
@@ -1,25 +1,26 @@
 module "primary-cluster" {
-  name                   = "my-cluster"
-  source                 = "russmedia/kubernetes-cluster/google"
-  version                = "2.0.0"
-  region                 = "us-east1"
-  zones                  = "us-east1-c"
-  project                = "linux-classs"
-  environment            = "Dev"
-  min_master_version     = "1.11.8"
+  name               = "my-cluster"
+  source             = "russmedia/kubernetes-cluster/google"
+  version            = "2.0.0"
+  region             = "us-east1"
+  zones              = "us-east1-c"
+  project            = "linux-classs"
+  environment        = "Dev"
+  min_master_version = "1.11.8"
+
   #node_pools             = ""
 
-
-node_pools = [
-  {
-    name                = "default-pool"
-    initial_node_count  = 2
-    min_node_count      = 2
-    max_node_count      = 3
-    version             = "1.11.8-gke.4"
-    image_type          = "COS"
-    machine_type        = "n1-highmem-4"
-    preemptible         = true
-    tags                = "Hello"
-  },
-]
+  node_pools = [
+    {
+      name               = "default-pool"
+      initial_node_count = 2
+      min_node_count     = 2
+      max_node_count     = 3
+      version            = "1.11.8-gke.4"
+      image_type         = "COS"
+      machine_type       = "n1-highmem-4"
+      preemptible        = true
+      tags               = "Hello"
+    },
+  ]
+}
