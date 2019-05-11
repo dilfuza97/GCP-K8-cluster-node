882c13dd223aa41091b6c097441e9fc58cfc7d54
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-05-11 13:23:20 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,30 +1,27 @@
-module "primary-cluster" {
-  name                                 = "my-cluster"
-  source                               = "russmedia/kubernetes-cluster/google"
-  version                              = "2.0.0"
-  region                               = "us-east1"
-  zones                                = ["us-east4-b"]
-  project                              = "linux-classs"
-  environment                          = "Dev"
-  min_master_version                   = "1.11.8"
-  network                              = "default"
-  nodes_subnet_ip_cidr_range           = "10.0.0.0/24"
-  nodes_subnet_container_ip_cidr_range = "172.0.0.0/16"
-  nodes_subnet_service_ip_cidr_range   = "10.0.0.0/24"
+provider "google" {
+  credentials   = "${file("${var.cpath}")}"   #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
+  project       = "${var.project}"
+  region        = "${var.region}"
+}
+resource "google_container_cluster" "cluster-fuchicorp-com" {
+    name                = "${var.cluster_name}"
+    network             = "default"
+    subnetwork          = "default"
+    zone                = "us-central1-a"
+    min_master_version  = "1.11.8-gke.6"
+    remove_default_node_pool = true
+    initial_node_count  = "${var.node_count}"
+}
 
-  #node_pools             = ""
+resource "google_container_node_pool" "node-fuchicorp-com" {
+  name       = "${var.node_name}"
+  location   = "us-central1-a"
+  cluster    = "${google_container_cluster.cluster-fuchicorp-com.name}"
+  node_count = "${var.node_count}"
 
-  node_pools = [
-    {
-      name               = "default-pool"
-      initial_node_count = 2
-      min_node_count     = 2
-      max_node_count     = 3
-      version            = "1.11.8-gke.4"
-      image_type         = "COS"
-      machine_type       = "n1-highmem-4"
-      preemptible        = true
-      tags               = "Hello"
-    },
-  ]
+    node_config {
+      machine_type      = "n1-standard-4"
+      # disk_size_gb = "${var.node_disk_size_gb}"
+      # image_type   = "${var.node_image_type}"
+  }
 }
